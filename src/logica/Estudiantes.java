d4982a2788a324b3ac8849f145c27e27f6b05a50
AdanOrtiz12 <adanortiz12b@gmail.com>
2024-06-30 18:14:35 -0500

    Update Estudiantes.java
    hecho por adan :v

--- src/logica/Estudiantes.java
+++ src/logica/Estudiantes.java
@@ -1,5 +1,37 @@
 package logica;
 
 public class Estudiantes {
+    private String nombre;
+    private String cedula;
+    private String carrera;
+    private double indiceAcademico;
+    private String sexo;
 
+    public Estudiantes(String nombre, String cedula, String carrera, double indiceAcademico, String sexo) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.carrera = carrera;
+        this.indiceAcademico = indiceAcademico;
+        this.sexo = sexo;
+    }
+
+    public double getIndiceAcademico() {
+        return indiceAcademico;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
 }
